abfa2e6f915b1f837495b3ec62dff47415cb4328
MendiMiz <mendelmizrahi770@gmail.com>
2024-09-01 10:43:28 +0300

    add Bubble sort function

---
+++ Sorting/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort(notSortedYet);
+            Console.WriteLine(string.Join(", ", notSortedYet));
+        }
+
+        public static List<int> notSortedYet = [1, 7, 2, -5, -15, 16, 3];
+        
+
+        public static List<int> BubbleSort(List<int> listToSort)
+        {
+            for(int i = 0; i < listToSort.Count; i++)
+            {
+                bool changed = false;
+                for (int j = 0; j < listToSort.Count - i - 1; j++)
+                {
+                    if (listToSort[j] > listToSort[j + 1])
+                    {
+                        int temp = listToSort[j];   
+                        listToSort[j] = listToSort[j + 1];
+                        listToSort[j + 1] = temp;   
+                        changed = true;
+                    }
+                }
+                if (!changed) return listToSort;
+            }
+            return listToSort;
+            
+        }
+
+
+    }
+}
